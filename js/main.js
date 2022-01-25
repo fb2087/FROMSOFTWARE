d06cb94fb4e3d88a33f7d1cb3b3b32cbc2f59b19
fb2087 <93234754+fb2087@users.noreply.github.com>
2022-01-25 16:16:51 +0900

    11111

--- js/main.js
+++ js/main.js
@@ -1,51 +1,97 @@
-$(function(){
-    $('.ds').hide();
-    $('.blood').hide();
-    $('.elden').hide();
-    $('.seki').hide();
-    $('.dera').hide();
+if (window.matchMedia("(min-width: 1200px)").matches) { 
 
-    $('.mainimg:nth-of-type(1)').click(function(){
-        $('.ds').fadeIn();
-    });
-    $('.mainimg:nth-of-type(2)').click(function(){
-        $('.blood').fadeIn();
-    });
-    $('.mainimg:nth-of-type(3)').click(function(){
-        $('.elden').fadeIn();
-    });
-    $('.mainimg:nth-of-type(4)').click(function(){
-        $('.seki').fadeIn();
-    });
-    $('.mainimg:nth-of-type(5)').click(function(){
-        $('.dera').fadeIn();
-    });
-    $('.cbtn').click(function(){
-        $('.ds').fadeOut();
-        $('.blood').fadeOut();
-        $('.elden').fadeOut();
-        $('.seki').fadeOut();
-        $('.dera').fadeOut();
-    });
-});
+    $(function(){
 
-$(function(){
-    $('.video').hide();
-    $('.elvi').click(function () {
-            $('.video').fadeIn();
-            $('.video iframe').attr("src","https://www.youtube.com/embed/l6pCyV7PnqI?autoplay=1&amp;mute=1");
+        // 1200이상 팝업
+        $('.ds').hide();
+        $('.blood').hide();
+        $('.elden').hide();
+        $('.seki').hide();
+        $('.dera').hide();
+    
+        $('.mainimg:nth-of-type(1)').click(function(){
+            $('.ds').fadeIn();
         });
-        $('.video').click(function () {
-            $('.video').fadeOut(); 
+        $('.mainimg:nth-of-type(2)').click(function(){
+            $('.blood').fadeIn();
         });
-});
-$(function(){
-    $('.video1').hide();
-    $('.sevi').click(function () {
-            $('.video1').fadeIn();
-            $('.video1 iframe').attr("src","https://www.youtube.com/embed/QFhaL4s10Iw?autoplay=1&amp;mute=1");
+        $('.mainimg:nth-of-type(3)').click(function(){
+            $('.elden').fadeIn();
         });
-        $('.video1').click(function () {
-            $('.video1').fadeOut(); 
+        $('.mainimg:nth-of-type(4)').click(function(){
+            $('.seki').fadeIn();
         });
-});
+        $('.mainimg:nth-of-type(5)').click(function(){
+            $('.dera').fadeIn();
+        });
+        $('.cbtn').click(function(){
+            $('.ds').fadeOut();
+            $('.blood').fadeOut();
+            $('.elden').fadeOut();
+            $('.seki').fadeOut();
+            $('.dera').fadeOut();
+        });
+  
+        // 1200이상 동영상
+        $('.video').hide();
+        $('.elvi').click(function () {
+                $('.video').fadeIn();
+                $('.video iframe').attr("src","https://www.youtube.com/embed/l6pCyV7PnqI?autoplay=1&amp;mute=1");
+            });
+            $('.video').click(function () {
+                $('.video').fadeOut(); 
+            });
+   
+
+        $('.video1').hide();
+        $('.sevi').click(function () {
+                $('.video1').fadeIn();
+                $('.video1 iframe').attr("src","https://www.youtube.com/embed/QFhaL4s10Iw?autoplay=1&amp;mute=1");
+            });
+            $('.video1').click(function () {
+                $('.video1').fadeOut(); 
+            });
+
+
+            //다크소울 이미지슬라이드
+            var i = 0;
+        function slide() {
+            i++;
+            if (i > $('.dssli div:last').index()) {
+                i = 0;
+            }
+            $('.dssli div').eq(i).fadeIn(1000);
+            $('.dssli div').eq(i-1).fadeOut(500);
+        }
+        setInterval(slide, 4000);
+
+ 
+        function fade() {
+            $('.derasli img:nth-of-type(2)').fadeIn(3000, function () {
+                $(this).fadeOut(3000);
+            });
+        }
+
+        setInterval(fade, 3000);
+    });
+
+} else { 
+    $(function(){
+        $(".menu").click(function(){
+            $(".a").slideToggle();
+        });
+    });
+    
+    $(window).resize(function(){
+        if(window.innerWidth<=1199){
+            $('.a').hide();
+        }else{
+            $('.a').show();
+        }
+    });
+   
+
+}
+
+
+
